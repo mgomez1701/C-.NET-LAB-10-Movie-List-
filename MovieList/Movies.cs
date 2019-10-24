cd1aeeb6b6dad7605e13f6c6cab7a8b992cf1944
Mike <michael005@me.com>
2019-10-24 19:09:02 -0400

    First Commit

---
+++ MovieList/Movies.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieList
+{
+ 
+    
+    class Movies
+    {
+       
+            
+        #region field
+        private string title;
+        private string category;
+        
+
+        #endregion
+
+        #region properties
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        #endregion
+
+        public Movies()
+        {
+
+        }
+        public Movies(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        public Movies(string _category)
+        {
+           
+            category = _category;
+        }
+        public static List<string> PrintCategoryList(List<string> catagory)
+        {
+            for (int i = 0; i < catagory.Count; i++)
+            {
+                Console.WriteLine($"{catagory[i]}");
+            }
+            return catagory;
+
+        }
+
+        //public static List<string> GetCatagories(List<Movies>movies)
+        //{
+        //    List<string> categories = new List<string>();
+        //    foreach(Movies flicks in movies)
+        //    {
+        //        if (!categories.Contains(flicks.Category))
+        //        {
+        //            categories.Add(flicks.Category);
+                    
+        //        }
+        //    }
+        //    return categories;
+        //}
+        
+
+    }
+}
---
+++ MovieList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movies> movieList = new List<Movies>
+            {
+                new Movies("Midsommer","Horror"),
+                new Movies("Heriditary", "Horror"),
+                new Movies("Once Upon a Time in Hollywood", "Drama"),
+                new Movies("The Dark Knight Rises", "Drama"),
+                new Movies("Surfs Up", "Animated"),
+                new Movies("Antz" , "Animated"),
+                new Movies("Intersteller", "Scifi"),
+                new Movies("Star Wars The Revenge of The Sith", "Scifi"),
+                new Movies("Wolf of Wall Street", "Drama"),
+                new Movies("Django Unchained Director Cut Unedited Version", "Drama"),
+
+            };
+
+            List<string> catagories = new List<string> // how would i make this a private method
+            {
+                new string ("Horror"),
+                new string ("Drama"),
+                new string ("Animation"),
+                new string ("Scifi"),
+
+            };
+
+            Console.WriteLine("Hello! We have many movies! What catagory are you interetsed in?");
+            Console.WriteLine("Please select from the following catagories");
+            Movies.PrintCategoryList(catagories);
+        }
+
+    }
+}
