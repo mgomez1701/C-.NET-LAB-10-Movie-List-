bfe6c4d6b63a23dec3628128db1b4dcb829371b8
Mike <michael005@me.com>
2019-10-25 00:46:03 -0400

    Lab Complete

--- MovieList/Movies.cs
+++ MovieList/Movies.cs
@@ -57,6 +57,109 @@ namespace MovieList
 
         }
 
+        public static int ValidateRange(string message, int min, int max) // used this instead of a try catch
+        {
+            int number = ParseInput(message);
+            if (number > min && number <= max)
+            {
+                return number;
+
+            }
+            else
+            {
+                return ValidateRange(message, min, max);
+            }
+
+        }
+
+        public static int ParseInput(string message)
+        {
+            try
+            {
+                string input = GetUserInput(message);
+                int number = int.Parse(input);
+                return number;
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Not in the correct format");
+                return ParseInput(message);
+            }
+            catch
+            {
+                return ParseInput(message);
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine().ToLower();
+        }
+
+        public static void HorrorMovies (int number)
+        {
+            int index = number;
+            
+            {
+                Console.WriteLine("Midsommer");
+                Console.WriteLine("Hereditary");
+            }
+        }
+
+        public static void DramaMovies(int number)
+        {
+            int index = number;
+
+            {
+                Console.WriteLine("Once Upon a Time in Hollywood");
+                Console.WriteLine("The Dark Knight Rises");
+                Console.WriteLine("Wolf of Wall Street");
+                Console.WriteLine("Django Unchained Director Cut Unedited Version");
+            }
+        }
+
+        public static void AnimatedMovies(int number)
+        {
+            int index = number;
+
+            {
+                Console.WriteLine("Surfs Up");
+                Console.WriteLine("Antz");
+                
+            }
+        }
+
+        public static void ScifiMovies(int number)
+        {
+            int index = number;
+
+            {
+                Console.WriteLine("Intersteller");
+                Console.WriteLine("Star Wars The Revenge of The Sith");
+            }
+        }
+
+        public static bool GetContinue()
+        {
+            bool letsGo = true;
+            Console.WriteLine($"Do you want to check out other movies in other catagories? Select Y/N");
+            string userContinue = Console.ReadLine();
+            switch(userContinue.ToLower())
+            {
+                case "y":
+                    return letsGo;
+                case "n":
+                    return false;
+                default:
+                    return GetContinue();
+            }
+
+        }
+
+
         //public static List<string> GetCatagories(List<Movies>movies)
         //{
         //    List<string> categories = new List<string>();
@@ -65,12 +168,12 @@ namespace MovieList
         //        if (!categories.Contains(flicks.Category))
         //        {
         //            categories.Add(flicks.Category);
-                    
+
         //        }
         //    }
         //    return categories;
         //}
-        
+
 
     }
 }
--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -19,21 +19,50 @@ namespace MovieList
                 new Movies("Star Wars The Revenge of The Sith", "Scifi"),
                 new Movies("Wolf of Wall Street", "Drama"),
                 new Movies("Django Unchained Director Cut Unedited Version", "Drama"),
-
             };
 
             List<string> catagories = new List<string> // how would i make this a private method
             {
-                new string ("Horror"),
-                new string ("Drama"),
-                new string ("Animation"),
-                new string ("Scifi"),
+                new string ("1).Horror"),
+                new string ("2).Drama"),
+                new string ("3).Animation"),
+                new string ("4).Scifi"),
 
             };
-
+            bool keepGoing = true;
             Console.WriteLine("Hello! We have many movies! What catagory are you interetsed in?");
-            Console.WriteLine("Please select from the following catagories");
-            Movies.PrintCategoryList(catagories);
+
+            while (keepGoing)
+            {
+                Movies.PrintCategoryList(catagories);
+                int index = Movies.ValidateRange(" You can choose a category by entering a number 1 - 4)", 0, catagories.Count);
+
+                if(index == 1)
+                {
+                    Movies.HorrorMovies(index);
+                }
+                else if (index == 2)
+                {
+                    Movies.DramaMovies(index);
+                }
+                else if (index == 3)
+                {
+                    Movies.AnimatedMovies(index);                    
+                }
+                else if (index == 4)
+                {
+                    Movies.ScifiMovies(index);
+                }
+                else
+                {
+                    Movies.ValidateRange(" You can choose a category by entering a number 1 - 4)", 0, catagories.Count);
+                }
+                keepGoing= Movies.GetContinue(); // here is where i have my false input if the user says no 
+                
+
+            }
+
+            
         }
 
     }
